10622d763686b974ffb50296005bada1e8c11eee
Paola Mérida <70529789+AnaMerida@users.noreply.github.com>
2024-03-15 18:26:28 -0600

    Add project files.

---
+++ HolaMundoSoftware/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HolaMundoSoftware
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola  Mundo!");
+        }
+    }
+}
